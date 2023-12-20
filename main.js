ee3eb32aab9849137bb2de8257f3eef64c2c0e77
Celso Zacarias <celso.zacarias@estudante.ufjf.br>
2023-12-20 18:42:21 -0300

    Adicionado dado único para os dois jogadores, afim de promover simplificação

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 import { calculaPontosColuna, atualizaTabuleiro } from "./jogo.js";
+var dadoJogador = document.getElementById('dice1');
+var jogarDado = document.getElementById('jogar-dado');
 
+jogarDado.onclick   = function () {rollDice();};
+
+function rollDice() {
+
+  var diceOne   = Math.floor((Math.random() * 6) + 1);
+  console.log(diceOne);
+
+  for (var i = 1; i <= 6; i++) {
+    dadoJogador.classList.remove('show-' + i);
+    if (diceOne === i) {
+      dadoJogador.classList.add('show-' + i);
+    }
+  }
+  dadoJogador.classList.add('show-' + diceOne);
+}
 gameLoop();
 function gameLoop(){
     atualizaTabuleiro();
